45251a3eac654468873b0e36d1f0bbcd631d2afb
Hobin Yoon <hobinyoon@gmail.com>
2017-05-21 13:41:55 -0400

    update

--- motivation/access-locations-most-freq-accessed-videos/loc.gnuplot
+++ motivation/access-locations-most-freq-accessed-videos/loc.gnuplot
@@ -2,8 +2,10 @@
 
 IN_FNS = system("echo $IN_FNS")
 IN_DN = system("echo $IN_DN")
+VIDS = system("echo $VIDS")
+VIEW_CNTS = system("echo $VIEW_CNTS")
+NUM_TWEETS = system("echo $NUM_TWEETS")
 OUT_FN = system("echo $OUT_FN")
-MAP = "~/work/crawl-twitter/worldmap/world_110m.txt"
 
 set print "-"
 #print sprintf("OUT_FN=%s", OUT_FN)
@@ -12,23 +14,27 @@ term_width=3.0
 set terminal pdfcairo enhanced size term_width in, (term_width*0.8) in
 set output OUT_FN
 
-set tmargin screen 0.89
+set tmargin screen 0.92
 
 # range_type
 # 0: World
 # 1: US
 # 2: New York
-rt=2
+rt=1
+
 
 if (rt == 0) {
 	set xtics nomirror tc rgb "black" autofreq -180,60
 	set ytics nomirror tc rgb "black" autofreq -90,30
+	MAP = "~/work/crawl-twitter/worldmap/world_110m.txt"
 } else { if (rt == 1) {
-	set xtics nomirror tc rgb "black" autofreq -180,30
-	set ytics nomirror tc rgb "black" autofreq -90,15
+	set xtics nomirror tc rgb "black" autofreq -180,10
+	set ytics nomirror tc rgb "black" autofreq -90,5
+	MAP = "~/work/castnet/misc/us-states-map/usa.txt"
 } else { if (rt == 2) {
 	set xtics nomirror tc rgb "black" autofreq -180,1
 	set ytics nomirror tc rgb "black" autofreq -90,1
+	MAP = "~/work/castnet/misc/us-states-map/usa.txt"
 } } }
 
 set grid xtics ytics front lc rgb "#808080"
@@ -38,8 +44,8 @@ if (rt == 0) {
 	set xrange[-180:180]
 	set yrange[-90:90]
 } else { if (rt == 1) {
-	set xrange[-130:-60]
-	set yrange[20:60]
+	set xrange[-127:-66]
+	set yrange[23:50]
 } else { if (rt == 2) {
 	#set xrange[-130:-60]
 	#set yrange[20:60]
@@ -47,16 +53,24 @@ if (rt == 0) {
 	set yrange[40:41.5]
 } } }
 
-#do for [i=1:words(IN_FNS)] {
-do for [i=1:10] {
-	vid = word(IN_FNS, i)[5:15]
+do for [i=1:words(IN_FNS)] {
+	vid = word(VIDS, i)
+	view_cnt = word(VIEW_CNTS, i)
+	num_tweets = word(NUM_TWEETS, i)
+	vtitle = system("echo $TITLE".i)
+
+	set title view_cnt . " " . num_tweets . " " . vid . "\n" . vtitle font ",6" offset 0,-1.5
+
 	fn = IN_DN . "/" . word(IN_FNS, i)
 
-	set title vid
-	# TODO: num_locs(tweets). views. video title.
+	if (rt == 0) {
+		plot MAP u 1:2 with filledcurves lw 1 fs solid noborder fc rgb "#F0F0F0" not, \
+			fn u 1:2 w p pt 6 pointsize 0.001 lc rgb "red" not
+	} else { if (rt >= 1) {
+		plot MAP u 2:1 with filledcurves lw 1 fs solid noborder fc rgb "#F0F0F0" not, \
+			MAP u 2:1 with l lw 1 lc rgb "#E0E0E0" not, \
+			fn u 1:2 w p pt 6 pointsize 0.006 lc rgb "red" not
+	} }
 
-	plot \
-	MAP u 1:2 with filledcurves lw 1 fs solid noborder fc rgb "#F0F0F0" not, \
-	fn u 1:2 w p pt 6 pointsize 0.001 lc rgb "red" not
-	#print sprintf("%d", i)
+	print sprintf("%d", i)
 }
--- motivation/access-locations-most-freq-accessed-videos/plot.py
+++ motivation/access-locations-most-freq-accessed-videos/plot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import locale
 import os
 import re
 import sys
@@ -9,9 +10,28 @@ import Cons
 import Util
 
 
+_dn_acc_loc = "%s/gen-data/.result/acc-locs" % os.path.dirname(__file__)
+
 def main(argv):
-	dn_in = "%s/gen-data/.result/acc-locs" % os.path.dirname(__file__)
-	GenInFiles(dn_in)
+	GenInputFiles(_dn_acc_loc)
+
+	locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+
+	# {video_id: VideoInfo}
+	vid_vi = GetVideoInfo()
+
+	vids = []
+	view_cnts = []
+	titles = []
+	num_tweets = []
+	for fn in _in_fns:
+		vid = fn[4:]
+		if vid not in vid_vi:
+			raise RuntimeError("Unexpected")
+		vids.append(vid)
+		view_cnts.append(locale.format("%d", int(vid_vi[vid].view_cnt), grouping=True))
+		titles.append(vid_vi[vid].title)
+		num_tweets.append(locale.format("%d", GetNumTweets(fn), grouping=True))
 
 	dn_out = "%s/.result" % os.path.dirname(__file__)
 	Util.MkDirs(dn_out)
@@ -21,7 +41,13 @@ def main(argv):
 	with Cons.MT("Plotting YouTube video access locations ..."):
 		env = os.environ.copy()
 		env["IN_FNS"] = " ".join(_in_fns)
-		env["IN_DN"] = dn_in
+		env["IN_DN"] = _dn_acc_loc
+		env["VIDS"] = " ".join(vids)
+		env["VIEW_CNTS"] = " ".join(view_cnts)
+		env["NUM_TWEETS"] = " ".join(num_tweets)
+		for i in range(len(titles)):
+			# Limit the title to 80 chars
+			env["TITLE%d" % (i+1)] = titles[i][0:80]
 		env["OUT_FN"] = fn_out
 		Util.RunSubp("gnuplot %s/loc.gnuplot" % os.path.dirname(__file__), env=env)
 		Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
@@ -29,7 +55,7 @@ def main(argv):
 
 _in_fns = []
 
-def GenInFiles(dn):
+def GenInputFiles(dn):
 	global _in_fns
 
 	ranks = []
@@ -60,6 +86,41 @@ def GenInFiles(dn):
 		return
 
 	raise RuntimeError("Implement calling the data generator")
+
+
+def GetVideoInfo():
+	vid_vi = {}
+	fn = "%s/work/crawl-twitter/tools/get-youtube-video-details/get-youtube-video-details/video-info-concise" % os.path.expanduser("~")
+	with open(fn) as fo:
+		for line in fo:
+			t = re.split(r" +", line.strip())
+			# chZmtjD8rxw 1154       5142   hd    PT5M49S M.O.D. - I Got Bitches
+			if len(t) < 6:
+				raise RuntimeError("Unexpected")
+			vid = t[0]
+			view_cnt = t[2]
+			title = " ".join(t[5:])
+			vid_vi[vid] = VideoInfo(view_cnt, title)
+	return vid_vi
+
+
+def GetNumTweets(fn0):
+	fn = "%s/%s" % (_dn_acc_loc, fn0)
+	num_tweets = 0
+	with open(fn) as fo:
+		for line in fo:
+			if len(line) == 0:
+				continue
+			if line[0] == "#":
+				continue
+			num_tweets += 1
+	return num_tweets
+
+
+class VideoInfo:
+	def __init__(self, view_cnt, title):
+		self.view_cnt = view_cnt
+		self.title = title
 
 
 if __name__ == "__main__":

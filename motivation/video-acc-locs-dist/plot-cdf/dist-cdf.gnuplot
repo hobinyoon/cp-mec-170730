f053dc11a588f915315433778d5a423200823bfd
Hobin Yoon <hobinyoon@gmail.com>
2017-05-25 11:38:18 -0400

    update

--- motivation/video-acc-locs-dist/plot-cdf/dist-cdf.gnuplot
+++ motivation/video-acc-locs-dist/plot-cdf/dist-cdf.gnuplot
@@ -3,6 +3,9 @@
 FN_CC = system("echo $FN_CC")
 FN_CT = system("echo $FN_CT")
 FN_AP= system("echo $FN_AP")
+STAT_CT  = system("echo $STAT_CT")
+STAT_AP  = system("echo $STAT_AP")
+STAT_CDN = system("echo $STAT_CDN")
 FN_CDN = system("echo $FN_CDN")
 FN_OUT = system("echo $FN_OUT")
 
@@ -13,6 +16,7 @@ set xlabel "Distance (m)"
 set ylabel "CDF" offset 0.8,0
 
 set xtics nomirror tc rgb "black" format "10^{%T}" 
+set mxtics 10
 set ytics nomirror tc rgb "black" autofreq 0,0.2 format "%.1f"
 set mytics 2
 set tics back
@@ -21,11 +25,76 @@ set grid xtics ytics mytics back lc rgb "#808080"
 
 set logscale x
 
-set xrange [10:]
+X_MIN=10
+set xrange [X_MIN:50000000]
 set yrange [0:]
 
+titles = "CT AP CDN"
+do for [i=1:words(titles)] {
+	title_ = word(titles, i)
+
+	if (title_ eq "CT") {
+		s = STAT_CT
+	} else { if (title_ eq "AP") {
+		s = STAT_AP
+	} else { if (title_ eq "CDN") {
+		s = STAT_CDN
+	} } }
+
+	avg = word(s, 1) + 0.0
+	# 1p and 99p
+	x_l = word(s, 2) + 0.0
+	x_r = word(s, 3) + 0.0
+	_y = word(s, 4) + 0.0
+
+	# OL: orthogonal lines
+	# HB: horizontal bar with 1p, avg, and 99p
+	style_ = "OL"
+	#style_ = "HB"
+
+	if (style_ eq "OL") {
+		LW1 = 6
+		set arrow from avg,0 to avg,1.0 nohead lc rgb "blue" lw LW1 lt 0
+		if (_y != -1) {
+			set obj circle at avg, _y size 1.2 fs solid fc rgb "blue" front
+		}
+
+		if (title_ eq "CT") {
+			set label title_ at avg,_y right offset -1,0
+		} else { if (title_ eq "AP") {
+			set label title_ at avg,_y left offset 1,0
+		} else { if (title_ eq "CDN") {
+			set label title_ at avg,0.5 right offset -1,0
+		} } }
+
+		#if (_y == -1) {
+		#	set arrow from avg,0 to avg,1.0 nohead lc rgb "blue" lw LW1 lt 0
+		#} else {
+		#	set arrow from avg,0 to avg,_y nohead lc rgb "blue" lw LW1 lt 0
+		#	set arrow from X_MIN,_y to avg,_y nohead lc rgb "blue" lw LW1 lt 0
+		#}
+	} else {
+		LW1 = 3
+		# Wing height
+		WH=0.02
+		set arrow from x_l,_y to x_r,_y nohead lc rgb "blue" lw LW1
+		set obj circle at avg, _y size 1.2 fs solid fc rgb "blue" front
+		set arrow from x_l,_y-WH to x_l,_y+WH nohead lc rgb "blue" lw LW1
+		set arrow from x_r,_y-WH to x_r,_y+WH nohead lc rgb "blue" lw LW1
+		set label title_ at x_r,_y offset 1,0
+	}
+}
+
+LW=3
+
 plot \
-FN_CC u 1:2 w l lw 2 t "CC", \
-FN_CT u 1:2 w l lw 2 t "CT", \
-FN_AP u 1:2 w l lw 2 t "AP", \
-FN_CDN u 1:2 w l lw 2 t "CDN", \
+FN_CC u 1:2 w l lw LW lc rgb "red" not, \
+
+# Move factor along x-axis and y-axis
+#mx=1.3
+#my=0.02
+#FN_CC u ((CT0 <= $1 && $1 <= CT1) ? $1/mx : 1/0):($2 + my) w l lw LW lc rgb "blue" not
+
+#FN_CT u 1:2 w l lw 2 t "CT", \
+#FN_AP u 1:2 w l lw 2 t "AP", \
+#FN_CDN u 1:2 w l lw 2 t "CDN", \
--- motivation/video-acc-locs-dist/plot-cdf/plot-cdf.py
+++ motivation/video-acc-locs-dist/plot-cdf/plot-cdf.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+import pprint
+import re
 import sys
 
 sys.path.insert(0, "%s/work/mutant/ec2-tools/lib/util" % os.path.expanduser("~"))
@@ -9,6 +11,7 @@ import Util
 
 
 _dn_result = "%s/.result" % os.path.dirname(__file__)
+_fn_cc = None
 
 
 def main(argv):
@@ -17,7 +20,8 @@ def main(argv):
 	dn = "%s/work/mec-node-placement/motivation/video-acc-locs-dist" % os.path.expanduser("~")
 
 	# Distance CDF to clustering centers, nearest CTs, APs, and CDNs.
-	fn_cc = "%s/to-clustering-center/.result/001-Y4MnpzG5Sqc-dist-to-clustering-center-cdf" % dn
+	global _fn_cc
+	_fn_cc = "%s/to-clustering-center/.result/001-Y4MnpzG5Sqc-dist-to-clustering-center-cdf" % dn
 	dn1 = "%s/to-closest-infra-node/gen-dist-cdf/.result" % dn
 	fn_ct = "%s/video-acc-dist-to-closest-ct-cdf" % dn1
 	fn_ap = "%s/video-acc-dist-to-closest-ap-cdf" % dn1
@@ -26,13 +30,71 @@ def main(argv):
 
 	with Cons.MT("Plotting clustered locations ..."):
 		env = os.environ.copy()
-		env["FN_CC"] = fn_cc
+		env["FN_CC"] = _fn_cc
 		env["FN_CT"] = fn_ct
 		env["FN_AP"] = fn_ap
 		env["FN_CDN"] = fn_cdn
+		env["STAT_CT"]  = _GetStatInStr(fn_ct)
+		env["STAT_AP"]  = _GetStatInStr(fn_ap)
+		env["STAT_CDN"] = _GetStatInStr(fn_cdn)
 		env["FN_OUT"] = fn_out
 		Util.RunSubp("gnuplot %s/dist-cdf.gnuplot" % os.path.dirname(__file__), env=env, measure_time=True)
 		Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
+
+
+def _GetStatInStr(fn):
+	stat = {}
+	with open(fn) as fo:
+		for line in fo:
+			if len(line) == 0:
+				continue
+			if line[0] != "#":
+				continue
+			#Cons.P(line)
+			t = re.split(r" +", line.strip())
+			if len(t) != 3:
+				raise RuntimeError("Unexpected")
+			# avg 1316.29
+			k = t[1]
+			v = float(t[2])
+			stat[k] = v
+	#return stat
+
+	# Calc y-coord. -1, if no intersection.
+	y = _Y(stat["avg"])
+
+	return " ".join(str(i) for i in [stat["avg"], stat["1p"], stat["99p"], y])
+
+
+_dist_to_cc_cdf = None
+def _Y(x):
+	global _dist_to_cc_cdf
+	if _dist_to_cc_cdf is None:
+		_dist_to_cc_cdf = {}
+		with open(_fn_cc) as fo:
+			for line in fo:
+				if len(line) == 0:
+					continue
+				if line[0] == "#":
+					continue
+				t = line.strip().split(" ")
+				if len(t) != 2:
+					raise RuntimeError("Unexpected")
+				dist = float(t[0])
+				cdf = float(t[1])
+				_dist_to_cc_cdf[dist] = cdf
+	d_prev = None
+	cdf_prev = None
+	for d, cdf in sorted(_dist_to_cc_cdf.iteritems()):
+		if d_prev is not None:
+			if (d_prev <= x) and (x <= d):
+				# y - cdf_prev : cdf - cdf_prev = x - d_prev : d - d_prev
+				# y - cdf_prev = (cdf - cdf_prev) * (x - d_prev) / (d - d_prev)
+				# y = (cdf - cdf_prev) * (x - d_prev) / (d - d_prev) + cdf_prev
+				return (cdf - cdf_prev) * (x - d_prev) / (d - d_prev) + cdf_prev
+		d_prev = d
+		cdf_prev = cdf
+	return -1
 
 
 if __name__ == "__main__":

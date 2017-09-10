be91218e3d368697021dcbaf5cac5f4637a99367
Hobin Yoon <hobinyoon@gmail.com>
2017-09-09 21:56:10 -0400

    new data

--- workloads/youtube/geographic-dist/cpp/plot-geographic-dist.py
+++ workloads/youtube/geographic-dist/cpp/plot-geographic-dist.py
@@ -9,7 +9,8 @@ import Util
 
 #_fn_in = "%s/work/cp-mec-data/170703-095232-tweets-97822" % os.path.expanduser("~")
 #_fn_in = "%s/work/cp-mec-data/170729-104532-tweets-983810" % os.path.expanduser("~")
-_fn_in = "%s/work/cp-mec-data/170904-110251-tweets-1146542" % os.path.expanduser("~")
+#_fn_in = "%s/work/cp-mec-data/170904-110251-tweets-1146542" % os.path.expanduser("~")
+_fn_in = "%s/work/cp-mec-data/170909-173940-tweets-1160643" % os.path.expanduser("~")
 
 _dist_sq_threshold = 0.03
 _dn_plot_data = "%s/.output" % os.path.dirname(__file__)
